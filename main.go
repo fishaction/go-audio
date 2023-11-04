66c3c0c1d4e8e29addc96c218d69387008df31aa
Ryota Nakaza <micrasoft.bf4@gmail.com>
2023-11-04 10:36:37 +0900

    exeファイルからアイコンを抽出pngとしてsave

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strconv"
 	"unsafe"
 
 	"github.com/go-ole/go-ole"
@@ -14,10 +15,9 @@ import (
 
 func main() {
 	log.SetFlags(0)
-	log.SetPrefix("error: ")
 
 	if err := run(os.Args); err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 }
 
@@ -45,23 +45,6 @@ func run(args []string) (err error) {
 	}
 	defer asm.Release()
 
-	/*
-
-		var asc *wca.IAudioSessionControl
-		if err = asm.GetAudioSessionControl(nil, 0, &asc); err != nil {
-			return
-		}
-		defer asc.Release()
-
-		var displayName string
-		if err = asc.GetDisplayName(&displayName); err != nil {
-			return
-		}
-		fmt.Println(displayName)
-
-
-	*/
-
 	var asm2 *wca.IAudioSessionManager2
 	if err = mmd.Activate(wca.IID_IAudioSessionManager2, wca.CLSCTX_ALL, nil, &asm2); err != nil {
 		return
@@ -83,27 +66,45 @@ func run(args []string) (err error) {
 
 	SHGetFileInfoW := windows.NewLazyDLL("shell32.dll").NewProc("SHGetFileInfoW")
 	if SHGetFileInfoW.Find() != nil {
-		log.Fatalln(SHGetFileInfoW.Find())
+		return SHGetFileInfoW.Find()
+	}
+
+	BitmapFromHICON := windows.NewLazyDLL("gdiplus.dll").NewProc("GdipCreateBitmapFromHICON")
+	if err := BitmapFromHICON.Find(); err != nil {
+		return err
+	}
+
+	SaveImageToFile := windows.NewLazyDLL("gdiplus.dll").NewProc("GdipSaveImageToFile")
+	if err := SaveImageToFile.Find(); err != nil {
+		return err
+	}
+
+	ExtractIconLibrary, err := windows.LoadDLL("./lib/ExtractIconLibrary.dll")
+	ExtractIconV3, err := ExtractIconLibrary.FindProc("ExtractIconV3")
+	if err != nil {
+		return err
 	}
 
 	for i := 0; i < cnt; i++ {
 		var session *wca.IAudioSessionControl
 		if err = sessionEnumerator.GetSession(i, &session); err != nil {
-			return
+			return err
 		}
 		defer session.Release()
 		var _ids *ole.IDispatch
 		var acs2 *wca.IAudioSessionControl2
 		if _ids, err = session.QueryInterface(wca.IID_IAudioSessionControl2); err != nil {
-			return
+			return err
 		}
 		acs2 = (*wca.IAudioSessionControl2)(unsafe.Pointer(_ids))
 		var processId uint32
 		acs2.GetProcessId(&processId)
 		var name string
 		acs2.GetDisplayName(&name)
-		fmt.Println(processId, name)
 		process, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION, false, processId)
+		if processId == 0 {
+			continue
+		}
 		if err != nil {
 			return err
 		}
@@ -116,32 +117,18 @@ func run(args []string) (err error) {
 			return res
 		}
 		filePath := windows.UTF16ToString(buff)
-		fmt.Println(filePath)
+		expPath := strconv.Itoa(int(processId)) + ".png"
 
-		shfile := SHFILEINFOA{}
-		size := reflect.TypeOf(shfile).Size()
-
-		flag := SHGFI_ICON | SHGFI_LARGEICON
-
-		var result uintptr
-		var dwFileAttributes int32 = -1
-
-		result, _, err = SHGetFileInfoW.Call(
-			(uintptr)(unsafe.Pointer(&filePath)),
-			(uintptr)(unsafe.Pointer(&dwFileAttributes)),
-			(uintptr)(unsafe.Pointer(&shfile)),
-			size,
-			(uintptr)(unsafe.Pointer(&flag)),
+		r, r2, err := ExtractIconV3.Call(
+			(uintptr)(unsafe.Pointer(windows.StringToUTF16Ptr(filePath))),
+			(uintptr)(unsafe.Pointer(windows.StringToUTF16Ptr(expPath))),
+			0x4,
 		)
-		if (int(result) == 0) && (err != nil) {
-			fmt.Println(err)
-		}
-		fmt.Println("status: ", err)
-		fmt.Println("HICON_Addr: ", (SHFILEINFOA)(shfile).hicon)
-	}
-	FromHICON := windows.NewLazyDLL("gdiplus.dll").NewProc("GdipCreateBitmapFromHICON")
-	if FromHICON.Find() != nil {
-		log.Fatalln(FromHICON.Find())
+		fmt.Println("r:", r)
+		fmt.Println("r2:", r2)
+		fmt.Println("err:", err)
+
+		fmt.Println("-----------")
 	}
 
 	return
@@ -149,13 +136,4 @@ func run(args []string) (err error) {
 
 const SHGFI_ICON uint32 = 0x000000100
 const SHGFI_LARGEICON uint32 = 0x000000000
-
-type HICON uintptr
-
-type SHFILEINFOA struct {
-	hicon         HICON
-	iIcon         int32
-	dwAttributes  uint32
-	szDisplayName [260]uint8
-	szTypeName    [80]uint8
-}
+const GMEM_FIXED uint32 = 0x0000
